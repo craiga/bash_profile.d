877f563a1eae93a06266303e6e5a757933475e67
Craig Anderson <craiga@craiga.id.au>
2018-03-08 14:49:56 +0000

    Add docker-compose-recreate alias

--- docker_compose_aliases.sh
+++ docker_compose_aliases.sh
@@ -14,3 +14,14 @@ docker_compose_django() {
   docker-compose run --rm "$1" python manage.py "${@:2}"
 }
 alias docker-compose-django='docker_compose_django'
+
+
+# Recreate a container by killing it and bringing it back up with --force-recreate
+# 
+#     docker-compose-recreate celery_worker
+
+docker_compose_recreate() {
+  docker-compose kill "$1" && docker-compose up --force-recreate -d "$1"
+}
+
+alias docker-compose-recreate="docker_compose_recreate"
