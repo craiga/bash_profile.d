8e11941485dc38aa37dd1e242dad4f1f8c44dd7f
Craig Anderson <craiga@craiga.id.au>
2018-03-16 15:41:17 +0000

    Ensure virtualenvwrapper uses Python 3

--- virtualenvwrapper.sh
+++ virtualenvwrapper.sh
@@ -1,4 +1,5 @@
 # http://virtualenvwrapper.readthedocs.io/en/latest/install.html#shell-startup-file
 export WORKON_HOME=$HOME/.ve
 export PROJECT_HOME=$HOME
+export VIRTUALENVWRAPPER_PYTHON=`which python3`
 source /usr/local/bin/virtualenvwrapper.sh
