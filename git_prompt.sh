3018b80f6c4f1d6ac0e7a2acbe14397d6767bfe1
Craig Anderson <craiga@craiga.id.au>
2018-06-01 16:17:05 +0100

    Add an emoji friend to my prompt

--- git_prompt.sh
+++ git_prompt.sh
@@ -3,4 +3,12 @@
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
     source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
-export GIT_PROMPT_END=' $ '
+
+FRIENDS=("🧐" "🤓" "👍" "🤘" "🖖" "👁" "👨‍💻" "🐶" "🐨" "🦄" "☄️" "🌼" "🌻"
+         "🌸" "🍃" "🌲" "🌳" "🌴" "🌱" "🌿" "☘️" "🍀" "🌎" "🌍" "🌏" "🌟"
+         "🌈" "☀️" "🍏" "🍎" "🍕" "🍭")
+NUM_FRIENDS=${#FRIENDS[@]}
+RANDOM_INDEX=$((RANDOM % NUM_FRIENDS))
+RANDOM_FRIEND=${FRIENDS[$RANDOM_INDEX]}
+
+export GIT_PROMPT_END=" $RANDOM_FRIEND $ "
