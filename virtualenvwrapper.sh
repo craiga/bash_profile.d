c1d3446d8dd65649c841f585399e1ee34382924c
Craig Anderson <craiga@craiga.id.au>
2018-07-02 13:12:27 +0100

    Add comment explaining what to do when that error message pops up

--- virtualenvwrapper.sh
+++ virtualenvwrapper.sh
@@ -1,4 +1,14 @@
 # http://virtualenvwrapper.readthedocs.io/en/latest/install.html#shell-startup-file
+
+# If Python has been updated and you see the following error:
+#
+#     Error while finding module specification for
+#     'virtualenvwrapper.hook_loader' (ModuleNotFoundError: No module named
+#     'virtualenvwrapper')
+#
+# …you may need to run `sudo -H pip3 install virtualenvwrapper` to re-install
+# virtualenvrapper.
+
 export WORKON_HOME=$HOME/.ve
 export PROJECT_HOME=$HOME
 export VIRTUALENVWRAPPER_PYTHON=`which python3`
