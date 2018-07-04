850a295d0ab6bae2231002d952183c652128bc6f
Craig Anderson <craiga@craiga.id.au>
2018-07-04 14:59:36 +0100

    rmpyguff

---
+++ rmpyguff.sh
@@ -0,0 +1,8 @@
+# rmpyguff
+#
+# Recursively removes all .pyc files and __pycache__ directories in the current
+# directory
+#
+# Based on https://gist.github.com/joelverhagen/3853540
+
+alias rmpyguff="find . | grep -E \"(__pycache__|\.pyc$)\" | xargs rm -rf"
