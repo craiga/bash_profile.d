85197fbc383c22a1f026219e1d796ee39aeeae64
Craig Anderson <craiga@craiga.id.au>
2018-07-30 10:43:35 +0100

    Use Solarized theme

--- 03_git_prompt.sh
+++ 03_git_prompt.sh
@@ -2,9 +2,10 @@
 
 # Set up with `brew install bash-git-prompt`.
 
+export GIT_PROMPT_END=" $EMOJI $ "
+export GIT_PROMPT_THEME='Solarized'
+
 if [ -f "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh" ]; then
   # shellcheck disable=SC1090
   source "$(brew --prefix)/opt/bash-git-prompt/share/gitprompt.sh"
 fi
-
-export GIT_PROMPT_END=" $EMOJI $ "
