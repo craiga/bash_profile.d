b4fc3c6872a59a051ecd79ad40032e6fae4c05cb
Craig Anderson <craiga@craiga.id.au>
2018-08-17 14:12:59 +0100

    Docker alias to quickly run a program

---
+++ 01_docker_aliases.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# docker aliases
+
+# Simply run a command on a given container.
+# 
+#     docker-quick-run python:2.7-alpine sh
+
+alias docker-quick-run='docker run -it --rm'
