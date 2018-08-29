509d4f2e92e44514678bc149dd4e99879d86f6d4
Craig Anderson <craiga@craiga.id.au>
2018-08-29 17:06:55 +0100

    Adding service logs to stdout alias

--- 01_docker_aliases.sh
+++ 01_docker_aliases.sh
@@ -7,3 +7,12 @@
 #     docker-quick-run python:2.7-alpine sh
 
 alias docker-quick-run='docker run -it --rm'
+
+# Output docker service logs to stdout
+#
+#     docker-service-logs-stdout web_celery-worker
+
+docker_service_logs_stdout() {
+  docker service logs --timestamps "$1" 2>&1
+}
+alias docker-service-logs-stdout='docker_service_logs_stdout'
